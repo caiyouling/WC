a04dc9655056ce47e02ac70e63f3c47e1fd08220
caiyouling <49674276+caiyouling@users.noreply.github.com>
2019-09-25 19:19:16 +0800

    Add files via upload

---
+++ test/dir/Test.java
@@ -0,0 +1,12 @@
+public static void main(String[] agrs){
+	system.out.print("Hello");
+	//这是一个注释行
+	
+	/*第二个注释*/
+	int i=1;
+	if(i=0)
+	{
+		123=i;
+		
+	}
+}/*这是一个注释*/
---
+++ test/normal.java
@@ -0,0 +1,5 @@
+public void main(String[] args){
+	//打印输出
+	System.out.println("我是一个正常的程序");
+
+}
